9fe74a7d9f92b24325410db4b655d712ea5d5d4a
Rejwanul Islam <irejwanul@live.com>
2024-09-29 10:48:43 +0600

    initial
    first commit

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Rejwanul Islam';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : '.NET, .Net MAUI, .Net Aspire';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '@irejwanul';
 
   return {
     name,
